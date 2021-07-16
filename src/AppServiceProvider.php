50d732b852db81f80336aa65f84d121a142c7164
mishu <mishu@xtelecom.ro>
2021-07-16 17:40:51 +0300

    removes log service provider

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -3,12 +3,16 @@
 namespace LaravelEnso\Api;
 
 use Illuminate\Support\ServiceProvider;
+use LaravelEnso\Api\Http\Middleware\ApiLogger;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         $this->mergeConfigFrom(__DIR__.'/../config/api.php', 'enso.api');
+
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        $this->app['router']->aliasMiddleware('api-action-logger', ApiLogger::class);
     }
 }
