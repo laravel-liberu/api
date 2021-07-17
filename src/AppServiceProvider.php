19cbed82fa0e0cf3c2fd7a0a110c0f75473f737a
mishu <mishu@xtelecom.ro>
2021-07-17 14:58:17 +0300

    adds core-api midleware group

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace LaravelEnso\Api;
 
 use Illuminate\Support\ServiceProvider;
 use LaravelEnso\Api\Http\Middleware\ApiLogger;
+use LaravelEnso\Core\Http\Middleware\VerifyActiveState;
+use LaravelEnso\Localisation\Http\Middleware\SetLanguage;
+use LaravelEnso\Permissions\Http\Middleware\VerifyRouteAccess;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,7 +15,17 @@ class AppServiceProvider extends ServiceProvider
         $this->mergeConfigFrom(__DIR__.'/../config/api.php', 'enso.api');
 
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+    }
 
+    public function register()
+    {
         $this->app['router']->aliasMiddleware('api-action-logger', ApiLogger::class);
+
+        $this->app['router']->middlewareGroup('core-api', [
+            VerifyActiveState::class,
+            ApiLogger::class,
+            VerifyRouteAccess::class,
+            SetLanguage::class,
+        ]);
     }
 }
