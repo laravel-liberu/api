db1bc9c12e2e67425651b16e0de654a4ca01fafb
mishu <mishu@xtelecom.ro>
2021-10-21 14:24:50 +0300

    adds a throttle helper

---
+++ src/Throttle.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace LaravelEnso\Api;
+
+class Throttle
+{
+    private int $debounce;
+    private float $time;
+
+    public function __construct(int $debounce)
+    {
+        $this->debounce = $debounce;
+        $this->time = $this->now();
+    }
+
+    public function __invoke(): self
+    {
+        $diff = $this->now() - $this->time;
+
+        if ($diff < $this->debounce) {
+            sleep($this->debounce - $diff);
+        }
+
+        $this->time = $this->now();
+
+        return $this;
+    }
+
+    private function now(): int
+    {
+        return (int) microtime(true);
+    }
+}
