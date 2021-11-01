a30272609bc2f7e5086485f97ee064035bc9a2e5
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2021-11-01 11:38:51 +0200

    removes nullable from duration

--- src/Upgrades/Duration.php
+++ src/Upgrades/Duration.php
@@ -3,11 +3,14 @@
 namespace LaravelEnso\Api\Upgrades;
 
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use LaravelEnso\Upgrade\Contracts\MigratesData;
+use LaravelEnso\Upgrade\Contracts\MigratesPostDataMigration;
 use LaravelEnso\Upgrade\Contracts\MigratesTable;
 use LaravelEnso\Upgrade\Helpers\Table;
 
-class Duration implements MigratesTable
+class Duration implements MigratesTable, MigratesData, MigratesPostDataMigration
 {
     public function isMigrated(): bool
     {
@@ -18,5 +21,16 @@ class Duration implements MigratesTable
     {
         Schema::table('api_logs', fn (Blueprint $table) => $table
             ->decimal('duration', 5, 2)->after('type')->nullable());
+    }
+
+    public function migrateData(): void
+    {
+        DB::table('api_logs')->update(['duration' => 0]);
+    }
+
+    public function migratePostDataMigration(): void
+    {
+        Schema::table('api_logs', fn (Blueprint $table) => $table
+            ->decimal('duration', 5, 2)->nullable(false)->change());
     }
 }
