fd293ca8880d4afbcbc98f72943733b60c84feae
Mihai <mihai.ocneanu@gmail.com>
2022-03-02 13:07:02 +0200

    update for L9

--- database/migrations/2021_07_01_100000_create_api_logs_table.php
+++ database/migrations/2021_07_01_100000_create_api_logs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateApiLogsTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -34,4 +34,4 @@ class CreateApiLogsTable extends Migration
     {
         Schema::dropIfExists('api_logs');
     }
-}
+};
